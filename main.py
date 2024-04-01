2b11b698c0031269db14fbf8c625dca57056cdd5
Michael B <biloshitskiy_m@mail.ru>
2024-04-02 00:26:27 +0300

    feat: logger 2.0

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from telegram import (
 from telegram.ext import (
     Application,
     ApplicationBuilder,
+    CallbackContext,
     CommandHandler,
-    CallbackContext
+    MessageHandler,
+    filters
 )
 import json
 import random
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(filename='main.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_file_handler = RotatingFileHandler('main.log', maxBytes=20 * 1024 * 1024, backupCount=5)
+log_file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(log_file_handler)
 
 with open('config.json', 'r') as f:
     config = json.loads(f.read())
@@ -20,9 +27,11 @@ with open('facts.txt', 'r') as f:
     facts = f.read().strip().split('\n')
 
 async def start_handle(update: Update, context: CallbackContext):
+    logger.info(f"@{update.message.from_user.username} : {update.message.text}")
     await update.message.reply_text(config["start_message"])
 
 async def send_fact(update: Update, context: CallbackContext):
+    logger.info(f"@{update.message.from_user.username} : {update.message.text}")
     random_fact = facts[random.randint(0, len(facts) - 1)]
     await update.message.reply_text(random_fact)
 
@@ -32,6 +41,9 @@ async def post_init(application: Application):
             BotCommand('/getfact', 'Новый факт про Анатолия Вассермана'),
         ]
     )
+
+async def log_message(update: Update, context: CallbackContext):
+    logger.info(f"@{update.message.from_user.username} : {update.message.text}")
 
 def run_bot() -> None:
     application = (
@@ -43,6 +55,7 @@ def run_bot() -> None:
     )
     application.add_handler(CommandHandler("start", start_handle))
     application.add_handler(CommandHandler('getfact', send_fact))
+    application.add_handler(MessageHandler(filters.TEXT, log_message))
     application.run_polling()
 
 run_bot()
